c21e8a00cd65e794a7bd5713c7773203fb15c535
Sigh <yxh644166727@gmail.com>
2022-10-06 23:09:12 -0400

    Time: 103 ms (91.62%), Space: 40.6 MB (6.15%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cs
@@ -0,0 +1,13 @@
+public class Solution 
+{
+    public bool CanMakeArithmeticProgression(int[] arr) 
+    {
+        Array.Sort(arr);
+        int d = arr[1] - arr[0];
+        
+        for(int i = 1; i < arr.Length; i++)
+            if(arr[i] != arr[i-1]+d) return false;
+        
+        return true;
+    }
+}
