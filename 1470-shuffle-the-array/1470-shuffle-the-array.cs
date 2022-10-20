92ca90b796f68d578dabaae18e04a937393f6b23
Sigh <yxh644166727@gmail.com>
2022-10-20 11:54:52 -0400

    Time: 242 ms (41.34%), Space: 43.6 MB (22.28%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cs
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cs
@@ -3,11 +3,10 @@ public class Solution
     public int[] Shuffle(int[] nums, int n) 
     {
         var output = new List<int>();
-        int half = nums.Length / 2;
         
-        for(int i = 0; i < half; i++)
+        for(int i = 0; i < n; i++)
         {
-            int j = half + i;
+            int j = n + i;
             output.Add(nums[i]);
             output.Add(nums[j]);
         }
