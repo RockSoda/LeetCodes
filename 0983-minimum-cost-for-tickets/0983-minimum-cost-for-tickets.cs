35f26bbd17ddda1233aabbe2bde398832c56f04b
Sigh <yxh644166727@gmail.com>
2022-10-20 17:42:52 -0400

    Time: 174 ms (10.00%), Space: 41.4 MB (5.71%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cs
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cs
@@ -3,27 +3,25 @@ public class Solution
     
     private int GetMin(int a, int b, int c) => Math.Min(Math.Min(a, b), c);
     
-    private int Recurse(int[] days, int[] costs, int index, int daysCovered, Dictionary<(int, int), int> memo)
+    private Dictionary<(int, int), int> memo = new Dictionary<(int, int), int>();
+    
+    public int MincostTickets(int[] days, int[] costs, int index = 0, int daysCovered = 0) 
     {
         if(index >= days.Length) return 0;
         
         if(memo.ContainsKey((index, daysCovered))) return memo[(index, daysCovered)];
         
         if(daysCovered >= days[index]) 
-            return memo[(index, daysCovered)] = Recurse(days, costs, index+1, daysCovered, memo);
+            memo[(index, daysCovered)] = MincostTickets(days, costs, index+1, daysCovered);
         else
         {
-            int one = Recurse(days, costs, index+1, days[index], memo) + costs[0];
-            int seven = Recurse(days, costs, index+1, days[index]+6, memo) + costs[1];
-            int thirty = Recurse(days, costs, index+1, days[index]+29, memo) + costs[2]; 
+            int one = MincostTickets(days, costs, index+1, days[index]) + costs[0];
+            int seven = MincostTickets(days, costs, index+1, days[index]+6) + costs[1];
+            int thirty = MincostTickets(days, costs, index+1, days[index]+29) + costs[2]; 
             
-            return memo[(index, daysCovered)] = GetMin(one, seven, thirty);
+            memo[(index, daysCovered)] = GetMin(one, seven, thirty);
         }
-    }
-    
-    public int MincostTickets(int[] days, int[] costs) 
-    {
-        var memo = new Dictionary<(int, int), int>();
-        return Recurse(days, costs, 0, 0, memo);
+        
+        return memo[(index, daysCovered)];
     }
 }
