7c10e8bdb5afe751a820a37937db3aeec59d97ce
Sigh <yxh644166727@gmail.com>
2022-10-21 11:42:41 -0400

    Time: 40 ms (60.00%), Space: 25.1 MB (30.00%) - LeetHub

--- 0754-reach-a-number/0754-reach-a-number.cs
+++ 0754-reach-a-number/0754-reach-a-number.cs
@@ -2,7 +2,7 @@ public class Solution
 {
     public int ReachNumber(int target) 
     {
-        if(target<0) target = -target;
+        if(target < 0) target = -target;
         
         int step = 0;
         int sum = 0;
@@ -10,8 +10,9 @@ public class Solution
         while(true)
         {
             step++;
-            sum = sum + step;
+            sum += step;
             
+                                // 1+2+3+4 > 8 && (10 - 8) % 2 == 0 ex. -1+2+3+4 = 8
             if(sum == target || (sum > target && (sum - target) % 2 == 0)) return step;
         }
         
