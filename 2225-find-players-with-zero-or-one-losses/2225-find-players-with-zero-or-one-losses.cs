0089e296583e5b94a14b2f3e48f87ee391e30b50
Sigh <yxh644166727@gmail.com>
2022-10-21 13:42:28 -0400

    Time: 1264 ms (30.98%), Space: 64.1 MB (74.65%) - LeetHub

---
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cs
@@ -0,0 +1,20 @@
+public class Solution 
+{
+    public IList<IList<int>> FindWinners(int[][] matches) 
+    {
+        var winners = matches.Select(m => m[0]).ToHashSet();
+        var winnerWhoLost = matches.Select(m => m[1]).Where(x => winners.Contains(x)).ToHashSet();
+        var zeroLose = winners.Where(x => !winnerWhoLost.Contains(x)).ToList();
+        zeroLose.Sort();
+        
+        var map = new Dictionary<int, int>();
+        foreach(var match in matches)
+        {
+            map[match[1]] = map.ContainsKey(match[1]) ? map[match[1]]+1 : 1;
+        }
+        var lostOnce = map.Where(kvp => kvp.Value == 1).Select(kvp => kvp.Key).ToList();
+        lostOnce.Sort();
+        
+        return new List<IList<int>>{zeroLose, lostOnce};
+    }
+}
