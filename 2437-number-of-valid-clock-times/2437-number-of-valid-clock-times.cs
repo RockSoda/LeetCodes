0e30f95f800d1019277221d86973fdda671e91a2
Sigh <yxh644166727@gmail.com>
2022-10-26 10:29:31 -0400

    Time: 109 ms (41.14%), Space: 34.8 MB (29.08%) - LeetHub

---
+++ 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.cs
@@ -0,0 +1,41 @@
+public class Solution 
+{
+    public int CountTime(string time) 
+    {
+        var ary = time.Split(":");
+        
+        int hours = 1;
+        //"??:xx"
+        if(ary[0][0] == '?' && ary[0][1] == '?') hours = 24;
+        else
+        {
+            //"?x:xx"
+            if(ary[0][0] == '?')
+            {
+                if(ary[0][1] - '0' <= 3) hours = 3;
+                else hours = 2;
+            }
+            
+            //"x?:xx"
+            if(ary[0][1] == '?')
+            {
+                if(ary[0][0] - '0' == 2) hours = 4;
+                else hours = 10;
+            }
+        }
+        
+        int minute = 1;
+        //"xx:??"
+        if(ary[1][0] == '?' && ary[1][1] == '?') minute = 60;
+        else
+        {
+            //"xx:?x"
+            if(ary[1][0] == '?') minute = 6;
+            
+            //"xx:x?"
+            if(ary[1][1] == '?') minute = 10;
+        }
+        
+        return hours*minute;
+    }
+}
