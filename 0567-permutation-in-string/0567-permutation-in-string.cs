17c44ef105dececf0f0176fc03f409f07a3420bd
Sigh <yxh644166727@gmail.com>
2022-10-28 17:17:19 -0400

    Time: 203 ms (32.39%), Space: 39 MB (32.39%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cs
@@ -0,0 +1,29 @@
+public class Solution 
+{
+    public bool CheckInclusion(string s1, string s2) 
+    {
+        var map = new int[26];
+        foreach(var c in s1) map[c-'a']++;
+        
+        for(int i = 0; i < s2.Length-s1.Length+1; i++)
+        {
+            var currMap = map.ToArray();
+            bool flag = true;
+            for(int j = 0; j < s1.Length; j++)
+            {
+                
+                if(--currMap[s2[i+j]-'a'] < 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if(flag) flag = currMap.Where(x => x == 0).ToList().Count == 26;
+            
+            if(flag) return true;
+        }
+        
+        return false;
+    }
+}
