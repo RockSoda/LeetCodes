c048e38a68dc9b0096b77f5aab10ef2a30b9f25f
Sigh <yxh644166727@gmail.com>
2022-10-31 13:09:02 -0400

    Time: 204 ms (16.82%), Space: 41.5 MB (27.10%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cs
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cs
@@ -4,40 +4,26 @@ public class Solution
     {
         for(int i = matrix.Length - 1; i >= 0; i--)
         {
-            bool flag = false;
             int j = 0;
             int prev = matrix[i][j];
             int r = i;
             while(r < matrix.Length && j < matrix[r].Length)
             {
-                if(matrix[r][j] != prev)
-                {
-                    flag = true;
-                    break;
-                }
+                if(matrix[r][j] != prev) return false;
                 prev = matrix[r++][j++];
             }
-            
-            if(flag) return false;
         }
         
         for(int j = 1; j < matrix[0].Length; j++)
         {
             int i = 0;
             int prev = matrix[i][j];
-            bool flag = false;
             int c = j;
             while(i < matrix.Length && c < matrix[i].Length)
             {
-                if(matrix[i][c] != prev)
-                {
-                    flag = true;
-                    break;
-                }
+                if(matrix[i][c] != prev) return false;
                 prev = matrix[i++][c++];
             }
-            
-            if(flag) return false;
         }
         
         return true;
