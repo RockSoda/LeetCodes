2a3480d79717ac7640fecbd7dc93ee7db2770b20
Sigh <yxh644166727@gmail.com>
2022-11-10 12:40:01 -0500

    Time: 105 ms (90.16%), Space: 42.6 MB (6.56%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cs
@@ -0,0 +1,34 @@
+public class Solution 
+{
+    public bool ValidateStackSequences(int[] pushed, int[] popped) 
+    {
+        var stk = new Stack<int>();
+        int indexPush = 0;
+        int indexPop = 0;
+        while(indexPush < pushed.Length && indexPop < popped.Length)
+        {
+            if(pushed[indexPush] == popped[indexPop])
+            {
+                indexPush++;
+                indexPop++;
+                continue;
+            }
+            
+            if(stk.Count > 0 && stk.Peek() == popped[indexPop])
+            {
+                indexPop++;
+                stk.Pop();
+                continue;
+            }
+            
+            stk.Push(pushed[indexPush++]);
+        }
+        
+        while(stk.Count > 0 && indexPop < popped.Length)
+        {
+            if(stk.Pop() != popped[indexPop++]) return false;
+        }
+        
+        return stk.Count == 0 && indexPop == popped.Length && indexPush == pushed.Length;
+    }
+}
