eaa5a1a9d432b8bd2fcaddf41863b8a7231e0201
Sigh <yxh644166727@gmail.com>
2022-11-11 11:29:38 -0500

    Time: 252 ms (58.33%), Space: 45.1 MB (30.43%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -1,26 +1,17 @@
 public class Solution 
 {
-    private void ShiftToBack(int[] nums, int index, int len)
-    {
-        for(int i = index; i < len-1; i++)
-        {
-            (nums[i], nums[i+1]) = (nums[i+1], nums[i]);
-        }
-    }
-    
     public int RemoveDuplicates(int[] nums) 
     {
-        int len = nums.Length;
-        for(int i = 1; i < len; i++)
+        int prev = nums[0];
+        
+        for(int i = 1; i < nums.Length; i++)
         {
-            if(nums[i-1] == nums[i])
-            {
-                ShiftToBack(nums, i, len);
-                i--;
-                len--;
-            }
+            if(prev == nums[i]) nums[i] = 101;
+            else prev = nums[i];
         }
         
-        return len;
+        Array.Sort(nums);
+        
+        return nums.Length - nums.Count(x => x == 101);
     }
 }
