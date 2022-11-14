666132d417153349a0050b388bf197f503ae49c1
Sigh <yxh644166727@gmail.com>
2022-11-14 11:47:50 -0500

    Time: 187 ms (5.46%), Space: 38 MB (65.53%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cs
@@ -0,0 +1,13 @@
+public class Solution 
+{
+    public char FindTheDifference(string s, string t) 
+    {
+        var map = new int[26];
+        foreach(var c in s) map[c-'a']++;
+        
+        foreach(var c in t)
+            if(--map[c-'a'] < 0) return c;
+        
+        return '-';
+    }
+}
