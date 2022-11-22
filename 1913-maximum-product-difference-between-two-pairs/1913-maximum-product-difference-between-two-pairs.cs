0fa0f2e4028f45d13d483e291900e49069e6ab9d
Sigh <yxh644166727@gmail.com>
2022-11-22 13:12:21 -0500

    Time: 226 ms (10.81%), Space: 40.2 MB (73.87%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cs
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cs
@@ -2,14 +2,7 @@ public class Solution
 {
     public int MaxProductDifference(int[] nums) 
     {
-        var list = nums.ToList();
-        var max1 = list.Max();
-        list.Remove(max1);
-        var max2 = list.Max();
-        list.Remove(max2);
-        var min1 = list.Min();
-        list.Remove(min1);
-        var min2 = list.Min();
-        return (max1*max2)-(min1*min2);
+        Array.Sort(nums);
+        return (nums[nums.Length-1]*nums[nums.Length-2])-(nums[0]*nums[1]);
     }
 }
