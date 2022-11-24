5134669c45fd5492856b76707aaa738fac5b1959
Sigh <yxh644166727@gmail.com>
2022-11-24 11:02:47 -0500

    Time: 363 ms (76.40%), Space: 39.8 MB (70.68%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cs
@@ -0,0 +1,34 @@
+public class Solution 
+{
+    private bool DFS(char[][] board, string word, int index, int i, int j)
+    {
+        if(index >= word.Length) return true;
+        if(i < 0 || i >= board.Length || j < 0 || j >= board[i].Length || board[i][j] != word[index]) return false;
+        
+        board[i][j] = '-';
+        
+        bool left = DFS(board, word, index+1, i, j - 1);
+        bool right = DFS(board, word, index+1, i, j + 1);
+        bool up = DFS(board, word, index+1, i - 1, j);
+        bool down = DFS(board, word, index+1, i + 1, j);
+        
+        board[i][j] = word[index];
+        
+        return left || right || up || down;
+    }
+    
+    public bool Exist(char[][] board, string word) 
+    {
+        for(int i = 0; i < board.Length; i++)
+        {
+            for(int j = 0; j < board[i].Length; j++)
+            {
+                var res = false;
+                if(board[i][j] == word.First()) res = DFS(board, word, 0, i, j);
+                if(res) return true;
+            }
+        }
+        
+        return false;
+    }
+}
