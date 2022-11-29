e242057a8bbbf22bdb87cc028c0f6aa750e120eb
Sigh <yxh644166727@gmail.com>
2022-11-29 11:18:15 -0500

    Time: 750 ms (31.26%), Space: 95.1 MB (33.82%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cs
@@ -0,0 +1,28 @@
+public class RandomizedSet 
+{
+    private Random rng;
+    private HashSet<int> hSet;
+    
+    public RandomizedSet() 
+    {
+        hSet = new HashSet<int>();
+        rng = new Random();
+    }
+    
+    public bool Insert(int val) => hSet.Add(val);
+    
+    public bool Remove(int val) => hSet.Remove(val);
+    
+    public int GetRandom() 
+    {
+        return hSet.ToList()[rng.Next(0, hSet.Count)];
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * bool param_1 = obj.Insert(val);
+ * bool param_2 = obj.Remove(val);
+ * int param_3 = obj.GetRandom();
+ */
