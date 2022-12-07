eccfda8c68185d005485dc5e94a0868930565577
Sigh <yxh644166727@gmail.com>
2022-12-07 12:40:44 -0500

    Time: 2140 ms (28.57%), Space: 211.4 MB (14.29%) - LeetHub

---
+++ 2080-range-frequency-queries/2080-range-frequency-queries.cs
@@ -0,0 +1,47 @@
+public class RangeFreqQuery 
+{
+    private Dictionary<int, List<int>> map;
+    
+    private Dictionary<(int, int, int), int> history;
+    
+    public RangeFreqQuery(int[] arr) 
+    {
+        history = new Dictionary<(int, int, int), int>();
+        map = new Dictionary<int, List<int>>();
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if(!map.ContainsKey(arr[i])) map[arr[i]] = new List<int>();
+            
+            map[arr[i]].Add(i);
+        }
+    }
+    
+    public int Query(int left, int right, int value)
+    {
+        if(history.ContainsKey((left, right, value))) return history[(left, right, value)];
+        
+        return history[(left, right, value)] = map.ContainsKey(value) ? SearchIndex(map[value], right, false) - SearchIndex(map[value], left, true) + 1 : 0;
+    }
+    
+    private int SearchIndex(List<int> indexes, int target, bool isLeft)
+    {
+        int left = 0;
+        int right = indexes.Count - 1;
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            
+            if(indexes[mid] > target) right = mid-1;
+            else if(indexes[mid] < target) left = mid+1;
+            else return mid;
+        }
+        
+        return isLeft ? left : right;
+    }
+}
+
+/**
+ * Your RangeFreqQuery object will be instantiated and called as such:
+ * RangeFreqQuery obj = new RangeFreqQuery(arr);
+ * int param_1 = obj.Query(left,right,value);
+ */
