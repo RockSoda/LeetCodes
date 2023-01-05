bb537bdc6d53126fcc88d0a4e6e2cf3c0155afc0
Sigh <yxh644166727@gmail.com>
2023-01-05 17:58:06 -0500

    Time: 448 ms (70.91%), Space: 59.7 MB (18.18%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cs
@@ -0,0 +1,20 @@
+public class Solution 
+{
+    public int FindMinArrowShots(int[][] points) 
+    {
+        points = points.OrderBy(point => point[1]).ToArray();
+        int arrows = 1, end = points[0][1];
+        
+        for(int i = 1; i < points.Length; i++)
+        {
+            if(points[i][0] > end)
+            {
+                arrows++;
+                end = points[i][1];
+            }
+        }
+        
+        return arrows;
+        
+    }
+}
