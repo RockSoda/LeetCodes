f695b47a333c180ea803c645d6c4b3e332fc2dba
Sigh <yxh644166727@gmail.com>
2023-01-09 15:13:38 -0500

    Time: 139 ms (90.87%), Space: 42.4 MB (7.05%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    private IList<int> list; 
+    
+    private void Traversal(TreeNode node)
+    {
+        if(node == null) return;
+        
+        list.Add(node.val);
+        Traversal(node.left);
+        Traversal(node.right);
+    }
+    
+    public IList<int> PreorderTraversal(TreeNode root) 
+    {
+        list = new List<int>();
+        Traversal(root);
+        return list;
+    }
+}
