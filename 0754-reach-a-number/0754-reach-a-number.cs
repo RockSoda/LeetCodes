48c2c5a5706be2da5bff49574e952023e8d16f0e
Sigh <yxh644166727@gmail.com>
2023-01-25 13:54:56 -0500

    Time: 29 ms (57.14%), Space: 26.6 MB (42.86%) - LeetHub

--- 0754-reach-a-number/0754-reach-a-number.cs
+++ 0754-reach-a-number/0754-reach-a-number.cs
@@ -2,18 +2,18 @@ public class Solution
 {
     public int ReachNumber(int target) 
     {
-        if(target < 0) target = -target;
+        if (target < 0) target = -target;
         
         int step = 0;
         int sum = 0;
         
-        while(true)
+        while (true)
         {
             step++;
             sum += step;
-            
                                 // 1+2+3+4 > 8 && (10 - 8) % 2 == 0 ex. -1+2+3+4 = 8
-            if(sum == target || (sum > target && (sum - target) % 2 == 0)) return step;
+            if (sum == target || (sum > target && (sum - target) % 2 == 0)) 
+                return step;
         }
         
         return int.MaxValue;
