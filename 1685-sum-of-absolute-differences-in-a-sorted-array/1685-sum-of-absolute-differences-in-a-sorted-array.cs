f8d3e493b6207db18ad545f4b92326be00658559
Sigh <yxh644166727@gmail.com>
2023-02-24 15:47:24 -0500

    Time: 287 ms (66.67%), Space: 63.5 MB (20.00%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cs
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cs
@@ -2,22 +2,23 @@ public class Solution
 {
     public int[] GetSumAbsoluteDifferences(int[] nums) 
     {
-        var prefixSum = new int[nums.Length];
+        int size = nums.Length;
+        var prefixSum = new int[size];
         prefixSum[0] = nums[0];
-        for(int i = 1; i < nums.Length; i++)
+        for(int i = 1; i < size; i++)
             prefixSum[i] = prefixSum[i-1] + nums[i];
         
-        var suffixSum = new int[nums.Length];
-        suffixSum[suffixSum.Length-1] = nums[nums.Length-1];
-        for(int i = nums.Length-2; i >= 0; i--)
+        var suffixSum = new int[size];
+        suffixSum[size-1] = nums[size-1];
+        for(int i = size-2; i >= 0; i--)
             suffixSum[i] = suffixSum[i+1] + nums[i];
         
-        var output = new int[nums.Length];
-        for(int i = 0; i < nums.Length; i++)
+        var output = new int[size];
+        for(int i = 0; i < size; i++)
         {
             int prefix = i-1 < 0 ? 0 : prefixSum[i-1];
-            int suffix = i+1 >= nums.Length ? 0 : suffixSum[i+1];
-            output[i] = (nums[i]*i - prefix) + (suffix - nums[i]*(nums.Length-i-1));
+            int suffix = i+1 >= size ? 0 : suffixSum[i+1];
+            output[i] = (nums[i]*i - prefix) + (suffix - nums[i]*(size-i-1));
         }
         
         return output;
