a6576cc185922c41bfb93ef39b511b379878eb59
Sigh <yxh644166727@gmail.com>
2023-03-21 14:24:23 -0400

    Time: 286 ms (47.06%), Space: 50.8 MB (94.12%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cs
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cs
@@ -3,20 +3,17 @@ public class Solution
     private List<int> GetZeroIntervals(List<int> nums)
     {
         int start = -1;
+        
         var output = new List<int>();
+        
         for(int i = 0; i < nums.Count; i++)
         {
-            if(nums[i] == 0 && start == -1)
-            {
-                start = i;
-                continue;
-            }
+            if(nums[i] == 0 && start == -1) start = i;
             
-            if(nums[i] != 0 && start != -1)
+            else if(nums[i] != 0 && start != -1)
             {
                 output.Add(i-start);
                 start = -1;
-                continue;
             }
         }
         
@@ -37,6 +34,7 @@ public class Solution
         var input = nums.ToList();
         input.Add(1);
         var zeroIntervals = GetZeroIntervals(input);
+        
         long output = 0;
         foreach(var zeros in zeroIntervals)
             output += GetSum(zeros);
