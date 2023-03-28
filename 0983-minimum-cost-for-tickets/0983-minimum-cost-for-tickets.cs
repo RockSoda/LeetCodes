d35510945a9905f94c06a9888172102024a15a8c
Sigh <yxh644166727@gmail.com>
2023-03-28 12:51:14 -0400

    Time: 110 ms (6.15%), Space: 44 MB (6.15%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cs
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cs
@@ -1,28 +1,30 @@
 public class Solution 
 {
-    private Dictionary<(int, int), int> memo;
-    
     private int GetMin(int a, int b, int c) => Math.Min(Math.Min(a, b), c);
     
-    public int MincostTickets(int[] days, int[] costs, int index = 0, int daysCovered = 0) 
+    private int Helper(int[] days, int[] costs, int index, int daysCovered, Dictionary<(int, int), int> memo)
     {
-        if(memo == null) memo = new Dictionary<(int, int), int>();
-        
         if(index >= days.Length) return 0;
         
         if(memo.ContainsKey((index, daysCovered))) return memo[(index, daysCovered)];
         
         if(daysCovered >= days[index]) 
-            memo[(index, daysCovered)] = MincostTickets(days, costs, index+1, daysCovered);
+            memo[(index, daysCovered)] = Helper(days, costs, index+1, daysCovered, memo);
         else
         {
-            int one = MincostTickets(days, costs, index+1, days[index]) + costs[0];
-            int seven = MincostTickets(days, costs, index+1, days[index]+6) + costs[1];
-            int thirty = MincostTickets(days, costs, index+1, days[index]+29) + costs[2]; 
+            int one = Helper(days, costs, index+1, days[index], memo) + costs[0];
+            int seven = Helper(days, costs, index+1, days[index]+6, memo) + costs[1];
+            int thirty = Helper(days, costs, index+1, days[index]+29, memo) + costs[2]; 
             
             memo[(index, daysCovered)] = GetMin(one, seven, thirty);
         }
         
         return memo[(index, daysCovered)];
+    }
+    
+    public int MincostTickets(int[] days, int[] costs)
+    {
+        var memo = new Dictionary<(int, int), int>();
+        return Helper(days, costs, 0, 0, memo);
     }
 }
