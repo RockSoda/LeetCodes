7adbfef424c3d79fb7a5fe3e428fcc6f1d9cadd1
Sigh <yxh644166727@gmail.com>
2023-04-18 12:23:38 -0400

    Time: 73 ms (89.02%), Space: 37 MB (93.29%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cs
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cs
@@ -6,11 +6,8 @@ public class Solution
         int len = word1.Length > word2.Length ? word1.Length : word2.Length;
         for(int i = 0; i < len; i++)
         {
-            char c1 = i < word1.Length ? word1[i] : '-';
-            char c2 = i < word2.Length ? word2[i] : '-';
-            
-            if(c1 != '-') sb.Append(c1);
-            if(c2 != '-') sb.Append(c2);
+            if(i < word1.Length) sb.Append(word1[i]);
+            if(i < word2.Length) sb.Append(word2[i]);
         }
         
         return sb.ToString();
