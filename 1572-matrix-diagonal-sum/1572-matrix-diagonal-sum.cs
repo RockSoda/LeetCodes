ba67cafdd6954774ee1bfb933a1c0e77132d52fb
Sigh <yxh644166727@gmail.com>
2023-05-08 11:03:48 -0400

    Time: 101 ms (52.20%), Space: 42.2 MB (8.29%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cs
@@ -0,0 +1,18 @@
+public class Solution 
+{
+    public int DiagonalSum(int[][] mat) 
+    {
+        int sum = 0;
+        for(int i = 0; i < mat.Length; i++)
+        {
+            sum += mat[i][i];
+            sum += mat[i][mat.Length-i-1];
+        }
+        
+        if(mat.Length % 2 == 0) return sum;
+        
+        sum -= mat[mat.Length/2][mat.Length/2];
+        
+        return sum;
+    }
+}
