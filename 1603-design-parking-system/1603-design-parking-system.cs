700eb0653f3c2caed2f2753e4104fc8e5aedab6e
Sigh <yxh644166727@gmail.com>
2023-05-29 16:41:28 -0400

    Time: 203 ms (5.38%), Space: 59.4 MB (34.41%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cs
+++ 1603-design-parking-system/1603-design-parking-system.cs
@@ -1,37 +1,17 @@
 public class ParkingSystem 
 {
-    private int _big;
-    private int _medium;
-    private int _small;
-
+    private int[] _parkingLot;
     
     public ParkingSystem(int big, int medium, int small) 
     {
-        _big = big;
-        _medium = medium;
-        _small = small;
+        _parkingLot = new int[3];
+        _parkingLot[0] = big;
+        _parkingLot[1] = medium;
+        _parkingLot[2] = small;
     }
     
-    public bool AddCar(int carType) 
-    {
-        switch(carType)
-        {
-            case 1:
-                if(_big <= 0) return false;
-                _big--;
-                return true;
-            case 2:
-                if(_medium <= 0) return false;
-                _medium--;
-                return true;
-            case 3:
-                if(_small <= 0) return false;
-                _small--;
-                return true;
-            default:
-                return false;
-        }
-    }
+    public bool AddCar(int carType) =>
+        _parkingLot[carType-1]-- > 0;
 }
 
 /**
