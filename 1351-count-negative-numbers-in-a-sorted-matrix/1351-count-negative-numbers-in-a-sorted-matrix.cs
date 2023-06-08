197a2b41681c589587ff62419ea6f302866bf127
Sigh <yxh644166727@gmail.com>
2023-06-08 10:58:21 -0400

    Time: 106 ms (40.55%), Space: 42.6 MB (91.71%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cs
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cs
@@ -1,12 +1,38 @@
 public class Solution 
 {
+    private int GetIndex(int[] ary)
+    {
+        int left = 0, right = ary.Length-1;
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            
+            if(ary[mid] < 0)
+            {
+                if(mid - 1 < 0) return 0;
+                
+                if(ary[mid-1] < 0) right = mid - 1;
+                else return mid;
+            }
+            else left = mid + 1;
+        }
+        
+        return -1;
+    }
+    
     public int CountNegatives(int[][] grid) 
     {
         
         int counter = 0;
         
         foreach(var row in grid)
-            counter += row.Count(cell => cell < 0);
+        {
+            int index = GetIndex(row);
+            
+            if(index == -1) continue;
+            
+            counter += (row.Length - index);
+        }
         
         return counter;
     }
