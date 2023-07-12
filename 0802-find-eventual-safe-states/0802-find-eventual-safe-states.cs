6ef44a5778b9958fded0d67a53cd14671177d90b
Sigh <yxh644166727@gmail.com>
2023-07-12 12:00:16 -0400

    Time: 1233 ms (5.81%), Space: 63 MB (22.09%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cs
@@ -0,0 +1,29 @@
+public class Solution 
+{
+    
+    public IList<int> EventualSafeNodes(int[][] graph) 
+    {
+        var terminals = new HashSet<int>();
+        
+        for(int i = 0; i < graph.Length; i++)
+            if(graph[i].Length == 0) terminals.Add(i);
+        
+        bool hasOperation = false;
+        do
+        {
+            hasOperation = false;
+            for(int i = 0; i < graph.Length; i++)
+            {
+                var path = new HashSet<int>(graph[i]);
+
+                if(path.IsSubsetOf(terminals))
+                    hasOperation = terminals.Add(i) || hasOperation;
+            }
+        }
+        while(hasOperation);
+        
+        var output = terminals.ToList();
+        output.Sort();
+        return output;
+    }
+}
