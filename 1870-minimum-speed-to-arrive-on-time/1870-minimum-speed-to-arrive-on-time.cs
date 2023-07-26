2e17ad2d7e35d58aa04282f6df6ad528464d57d5
Sigh <yxh644166727@gmail.com>
2023-07-26 14:42:05 -0400

    Time: 327 ms (73.33%), Space: 53 MB (13.33%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cs
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cs
@@ -4,27 +4,28 @@ public class Solution
     {
         if (hour <= dist.Length-1) return -1;
         
-        double GetTime(int mid)
+        double GetTime(int speed)
         {
             double time = 0;
             for (int i = 0; i < dist.Length-1; i++)
-                time += Math.Ceiling((double)dist[i] / mid);
+                time += Math.Ceiling((double)dist[i] / speed);
             
-            time += (double)dist.Last() / mid;
+            time += (double)dist.Last() / speed;
             
             return time;
         }
         
         int left = 1, right = (int)Math.Pow(10,7);
         
-        while(left < right)
+        while(left <= right)
         {
             int mid = left + (right - left) / 2;
             
             double time = GetTime(mid);
 
-            if (time <= hour) right = mid;
-            else left = mid+1;
+            if (time < hour) right = mid-1;
+            else if (time > hour) left = mid+1;
+            else return mid;
         }
         
         return left;
