23d97ab9112cd04a2cbfe3a721ca90a117f2bbda
Sigh <yxh644166727@gmail.com>
2023-08-17 17:06:38 -0400

    Time: 195 ms (90.91%), Space: 69.3 MB (18.64%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cs
@@ -0,0 +1,66 @@
+public class Solution 
+{
+    private int[][] res = null;
+    private int row;
+    private int col;
+
+    public int[][] UpdateMatrix(int[][] mat)
+    {
+        if (mat.Length == 0) return mat;
+
+        res = mat;
+        row = mat.Length;
+        col = mat[0].Length;
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (res[i][j] == 1 && !HasNeighborZero(i, j))
+                    res[i][j] = int.MaxValue;
+            }
+        }
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (res[i][j] != 1) continue;
+
+                DFS(i, j, 1);
+            }
+        }
+
+        return res;
+    }
+
+    private void DFS(int x, int y, int val)
+    {
+        if (x < 0 || x >= row || y < 0 || y >= col || res[x][y] < val) return;
+
+        res[x][y] = val;
+
+        val++;
+        DFS(x - 1, y, val); // top
+        DFS(x + 1, y, val); // bottom
+        DFS(x, y - 1, val); // left
+        DFS(x, y + 1, val); // right
+    }
+
+    private bool HasNeighborZero(int x, int y)
+    {
+        // top
+        if (x > 0 && res[x - 1][y] == 0) return true;
+
+        // bottom
+        if (x < row - 1 && res[x + 1][y] == 0) return true;
+
+        // left
+        if (y > 0 && res[x][y - 1] == 0) return true;
+
+        // right
+        if (y < col - 1 && res[x][y + 1] == 0) return true;
+
+        return false;
+    }
+}
