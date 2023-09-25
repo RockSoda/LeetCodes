8b90e3ed9fedbc42756fec36dbbac925956a550b
Sigh <yxh644166727@gmail.com>
2023-09-25 13:41:26 -0400

    Time: 75 ms (93.62%), Space: 39.6 MB (45.22%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cs
+++ 0389-find-the-difference/0389-find-the-difference.cs
@@ -3,10 +3,10 @@ public class Solution
     public char FindTheDifference(string s, string t) 
     {
         var map = new int[26];
+        
         foreach(var c in s) map[c-'a']++;
         
-        foreach(var c in t)
-            if(--map[c-'a'] < 0) return c;
+        foreach(var c in t) if(--map[c-'a'] < 0) return c;
         
         return '-';
     }
