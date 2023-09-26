d551f1099fe255adbfcf6935bad3bd1f03f5dead
Sigh <yxh644166727@gmail.com>
2023-09-26 11:34:01 -0400

    Time: 75 ms (52.63%), Space: 36.7 MB (100.00%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cs
@@ -0,0 +1,31 @@
+public class Solution 
+{
+    public string SmallestSubsequence(string s) 
+    {
+        var stk = new Stack<char>();
+        var map = new int[26];
+        var seen = new bool[26];
+        
+        for(int i = 0; i < s.Length; i++) map[s[i]-'a'] = i;
+        
+        for(int i = 0; i < s.Length; i++)
+        {
+            char cur = s[i];
+            
+            if(seen[cur-'a']) continue;
+            
+            while(stk.Count > 0 && cur < stk.Peek() && map[stk.Peek()-'a'] > i)
+            {
+                seen[stk.Peek()-'a'] = false;
+                stk.Pop();
+            }
+            stk.Push(cur);
+            seen[cur-'a'] = true;
+        }
+        
+        var ans = new StringBuilder();
+        while(stk.Count > 0) ans.Insert(0, stk.Pop());
+        
+        return ans.ToString();
+    }
+}
