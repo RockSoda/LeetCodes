3173420f7f67a837c4e509572a460169c3b1baeb
Sigh <yxh644166727@gmail.com>
2023-11-16 11:52:25 -0500

    Time: 84 ms (76.00%), Space: 40.1 MB (36.00%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cs
@@ -0,0 +1,17 @@
+public class Solution 
+{
+    public string FindDifferentBinaryString(string[] nums) 
+    {
+        var bits = nums.First().Length;
+        var size = (int)Math.Pow(2, bits);
+        var set = nums.ToHashSet();
+        
+        for (int i = 0; i < size; i++)
+        {
+            var curr = Convert.ToString(i, 2).PadLeft(bits, '0');
+            if (!set.Contains(curr)) return curr;
+        }
+        
+        return string.Empty;
+    }
+}
