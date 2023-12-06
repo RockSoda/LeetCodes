3573b7b43fd56f03e6fa2dd8f5fe1bf88bc19e11
Sigh <yxh644166727@gmail.com>
2023-12-06 13:01:11 -0500

    Time: 28 ms (10.81%), Space: 26.3 MB (91.89%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cs
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cs
@@ -2,15 +2,21 @@ public class Solution
 {
     public int TotalMoney(int n) 
     {
+        int GetSum (int start, int len)
+        {
+            int end = start + len - 1;
+            return ((start + end) * len) / 2;
+        }
+        
         int rounds = n / 7;
         int remaind = n % 7;
         
         int money = 0;
         
         for(int i = 1; i <= rounds; i++)
-            money += ((2 * i + 6) * 7) / 2;
+            money += GetSum(i, 7);
         
-        money += ((2 * (rounds + 1) + remaind - 1) * remaind) / 2;
+        money += GetSum(rounds+1, remaind);
         return money;
     }
 }
