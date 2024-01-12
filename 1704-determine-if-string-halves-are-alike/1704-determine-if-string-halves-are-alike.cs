0e36a18e4d3bb44a423deccf6e7e4009773ed527
Sigh <yxh644166727@gmail.com>
2024-01-12 11:59:50 -0500

    Time: 59 ms (82.24%), Space: 39.1 MB (16.82%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cs
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cs
@@ -3,14 +3,9 @@ public class Solution
     public bool HalvesAreAlike(string s) 
     {
         var vowels = new HashSet<char>{'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'};
-        var a = new List<char>();
-        var b = new List<char>();
-        
-        for(int i = 0; i < s.Length; i++)
-        {
-            if(i < s.Length / 2) a.Add(s[i]);
-            else b.Add(s[i]);
-        }
+        var half = s.Length/2;
+        var a = s.Take(half);
+        var b = s.Skip(half).Take(half);
         
         return a.Count(c => vowels.Contains(c)) == b.Count(c => vowels.Contains(c));
     }
