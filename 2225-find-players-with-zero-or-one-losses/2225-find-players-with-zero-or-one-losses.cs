fa51586e0dab52fbea6c6c48f0ebeb8dad8d5aeb
Sigh <yxh644166727@gmail.com>
2024-01-15 14:13:39 -0500

    Time: 793 ms (57.23%), Space: 131.6 MB (18.07%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cs
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cs
@@ -2,17 +2,17 @@ public class Solution
 {
     public IList<IList<int>> FindWinners(int[][] matches) 
     {
-        var winners = matches.Select(m => m[0]).ToHashSet();
-        var winnerWhoLost = matches.Select(m => m[1]).Where(x => winners.Contains(x)).ToHashSet();
-        var zeroLose = winners.Where(x => !winnerWhoLost.Contains(x)).ToList();
-        zeroLose.Sort();
-        
         var map = new Dictionary<int, int>();
         foreach(var match in matches)
+        {
+            if (!map.ContainsKey(match[0])) map[match[0]] = 0;
             map[match[1]] = map.ContainsKey(match[1]) ? map[match[1]]+1 : 1;
+        }
         
         var lostOnce = map.Where(kvp => kvp.Value == 1).Select(kvp => kvp.Key).ToList();
         lostOnce.Sort();
+        var zeroLose = map.Where(kvp => kvp.Value == 0).Select(kvp => kvp.Key).ToList();
+        zeroLose.Sort();
         
         return new List<IList<int>>{zeroLose, lostOnce};
     }
