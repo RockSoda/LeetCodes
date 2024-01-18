99e71eff7b99855308573ffa46c78e0049f9ff2b
Sigh <yxh644166727@gmail.com>
2024-01-18 12:41:08 -0500

    Time: 30 ms (8.27%), Space: 26.8 MB (33.32%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cs
@@ -0,0 +1,19 @@
+public class Solution 
+{
+    private int Recurse(int n, Dictionary<int, int> memo)
+    {
+        if(n == 1) return 1;
+        
+        if(n == 2) return 2;
+        
+        if(memo.ContainsKey(n)) return memo[n];
+        
+        return memo[n] = Recurse(n-1, memo) + Recurse(n-2, memo);
+    }
+    
+    public int ClimbStairs(int n) 
+    {
+        var memo = new Dictionary<int, int>();
+        return Recurse(n, memo);
+    }
+}
