8df62d5bc470b6009bf03acef04199af6f5526ee
Sigh <yxh644166727@gmail.com>
2024-02-01 12:53:30 -0500

    Time: 448 ms (7.50%), Space: 131.8 MB (10.00%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cs
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cs
@@ -3,19 +3,16 @@ public class Solution
     public int[][] DivideArray(int[] nums, int k) 
     {
         Array.Sort(nums);
-        int groupIndex = 0, size = nums.Length;
-        var result = new int[size / 3][];
+        int size = nums.Length;
+        var result = new List<int[]>();
         
         for (int i = 0; i < size; i += 3) 
         {
             if (i + 2 < size && nums[i + 2] - nums[i] <= k) 
-            {
-                result[groupIndex] = new int[]{ nums[i], nums[i + 1], nums[i + 2] };
-                groupIndex++;
-            }
+                result.Add(new int[]{ nums[i], nums[i + 1], nums[i + 2] });
             else return new int[0][];
         }
         
-        return result;
+        return result.ToArray();
     }
 }
