394e5a4e67c1bb5ee46c84e02b8918f7eefbbe07
Sigh <yxh644166727@gmail.com>
2024-02-12 13:06:43 -0500

    Time: 707 ms (5.55%), Space: 53 MB (22.22%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cs
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cs
@@ -3,12 +3,12 @@ public class Solution
     private int GetMax(int a, int b, int c, int d, int e, int f, int g, int h, int i)=>
         Math.Max(Math.Max(Math.Max(Math.Max(a, b), Math.Max(c, d)), Math.Max(Math.Max(e, f), Math.Max(g, h))), i);
     
-    private int DP(int[][] grid, int i, int j1, int j2, int[][][] memo)
+    private int DP(int[][] grid, int i, int j1, int j2, Dictionary<(int, int, int), int> memo)
     {
         if(i < 0 || i >= grid.Length ||  j1 < 0 || j1 >= grid[i].Length || j2 < 0 || j2 >= grid[i].Length)
             return -9999999;
         
-        if(memo[i][j1][j2] != -1) return memo[i][j1][j2];
+        if(memo.ContainsKey((i, j1, j2))) return memo[(i, j1, j2)];
         
         int curr1 = grid[i][j1];
         int curr2 = grid[i][j2];
@@ -17,7 +17,7 @@ public class Solution
         
         if(i == grid.Length-1) return curr1+curr2;
         
-        return memo[i][j1][j2] = GetMax(
+        return memo[(i, j1, j2)] = GetMax(
             DP(grid, i+1, j1-1, j2-1, memo)+curr1+curr2,
             DP(grid, i+1, j1-1, j2, memo)+curr1+curr2,
             DP(grid, i+1, j1-1, j2+1, memo)+curr1+curr2,
@@ -31,16 +31,7 @@ public class Solution
     
     public int CherryPickup(int[][] grid) 
     {
-        var memo = new int[grid.Length][][];
-        for(int i = 0; i < memo.Length; i++)
-        {
-            memo[i] = new int[grid[0].Length][];
-            for(int j = 0; j < memo[i].Length; j++)
-            {
-                memo[i][j] = new int[grid[0].Length];
-                Array.Fill(memo[i][j], -1);
-            }
-        }
+        var memo = new Dictionary<(int, int, int), int>();
         
         return DP(grid, 0, 0, grid[0].Length-1, memo);
     }
