d4935a1bc57bae4aa53350fcaf78de906bb843ef
Sigh <yxh644166727@gmail.com>
2024-03-25 15:54:34 -0400

    Time: 126 ms (22.46%), Space: 51.1 MB (36.23%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cs
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cs
@@ -2,21 +2,28 @@ public class Solution
 {
     public IList<IList<int>> GroupThePeople(int[] groupSizes) 
     {
-        var map = new Dictionary<int, List<int>>();
+        var map = new Dictionary<int, List<IList<int>>>();
         for (int i = 0; i < groupSizes.Length; i++)
         {
             var groupSize = groupSizes[i];
-            if (!map.ContainsKey(groupSize)) map[groupSize] = new List<int>();
-            map[groupSize].Add(i);
+            if (!map.ContainsKey(groupSize)) 
+            {
+                map[groupSize] = new List<IList<int>>();
+                map[groupSize].Add(new List<int>());
+            }
+            
+            var currList = map[groupSize].Last();
+            if(currList.Count == groupSize)
+            {
+                map[groupSize].Add(new List<int>());
+                currList = map[groupSize].Last();
+            }
+            currList.Add(i);
         }
         
         var output = new List<IList<int>>();
         
-        foreach(var kvp in map)
-        {
-            var chunks = kvp.Value.Chunk(kvp.Key);
-            output.AddRange(chunks);
-        }
+        foreach(var kvp in map) output.AddRange(kvp.Value);
         
         return output;
     }
