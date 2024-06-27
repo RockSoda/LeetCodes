7f78dd5ee24c46e9533b118e0976eefcbd727823
Sigh <yxh644166727@gmail.com>
2024-06-27 13:21:37 -0400

    Time: 426 ms (5.80%), Space: 76 MB (27.54%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cs
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cs
@@ -2,17 +2,14 @@ public class Solution
 {
     public int FindCenter(int[][] edges) 
     {
-        var set = new HashSet<int>();
         var map = new Dictionary<int, int>();
         foreach(var edge in edges)
         {
-            set.Add(edge[0]);
-            set.Add(edge[1]);
-            
             map[edge[0]] = map.ContainsKey(edge[0]) ? map[edge[0]]+1 : 1;
             map[edge[1]] = map.ContainsKey(edge[1]) ? map[edge[1]]+1 : 1;
         }
         
-        return map.ToList().Find(kvp => kvp.Value == set.Count - 1).Key;
+        var max = map.ToList().Max(kvp => kvp.Value);
+        return map.ToList().Find(kvp => kvp.Value == max).Key;
     }
 }
