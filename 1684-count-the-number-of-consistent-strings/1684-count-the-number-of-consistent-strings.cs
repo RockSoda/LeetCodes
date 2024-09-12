c1d9e9b73e362b9adefaadae5f7389dabd3a2e59
Sigh <yxh644166727@gmail.com>
2024-09-12 12:39:02 -0400

    Time: 93 ms (97.37%), Space: 54.9 MB (25.26%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cs
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cs
@@ -2,15 +2,7 @@ public class Solution
 {
     public int CountConsistentStrings(string allowed, string[] words) 
     {
-        var set = new HashSet<char>();
-        foreach(var c in allowed) set.Add(c);
-        
-        var counter = 0;
-        foreach(var word in words)
-        {
-            if(word.Any(c => !set.Contains(c))) continue;
-            counter++;
-        }
-        return counter;
+        var set = new HashSet<char>(allowed);
+        return words.Count(word => word.All(c => set.Contains(c)));
     }
 }
