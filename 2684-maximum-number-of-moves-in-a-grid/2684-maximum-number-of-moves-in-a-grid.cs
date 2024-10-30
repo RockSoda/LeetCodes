74eda52dbdadf6745118a6f6afe3b236a3be0024
Sigh <yxh644166727@gmail.com>
2024-10-30 11:58:27 -0400

    Time: 10 ms (100%), Space: 91.1 MB (83.33%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cs
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cs
@@ -1,33 +1,34 @@
 public class Solution 
 {
-    private int[][] CopyGrid(int[][] mat)
-    {
-        var output = new int[mat.Length][];
-        for(int i = 0; i < output.Length; i++)
-            output[i] = mat[i].ToArray();
-        return output;
-    }
-
     private int GetMax(int a, int b, int c) => Math.Max(a, Math.Max(b, c));
 
-    private int Traverse(int[][] mat, int i, int j, int prev)
+    private int Traverse(int[][] mat, int i, int j, int prev, int[][] memo)
     {
         if(i < 0 || i >= mat.Length || j < 0 || j >= mat[i].Length || prev >= mat[i][j]) return -1;
-        
+        if(memo[i][j] != -1) return memo[i][j];
+
         int curr = mat[i][j];
         mat[i][j] = 1000001;
 
-        return GetMax(Traverse(CopyGrid(mat), i-1, j+1, curr) + 1,
-                      Traverse(CopyGrid(mat), i, j+1, curr) + 1,
-                      Traverse(CopyGrid(mat), i+1, j+1, curr) + 1);
+        memo[i][j] = GetMax(Traverse(mat, i-1, j+1, curr, memo) + 1,
+                         Traverse(mat, i, j+1, curr, memo) + 1,
+                         Traverse(mat, i+1, j+1, curr, memo) + 1);
+        mat[i][j] = curr;
+        return memo[i][j];
     }
 
     public int MaxMoves(int[][] grid) 
     {
         int output = 0;
-        
+        var memo = new int[grid.Length][];
+        for(int i = 0; i < memo.Length; i++)
+        {
+            memo[i] = new int[grid[i].Length];
+            Array.Fill(memo[i], -1);
+        }
+
         for(int i = 0; i < grid.Length; i++)
-            output = Math.Max(Traverse(CopyGrid(grid), i, 0, 0), output);
+            output = Math.Max(Traverse(grid, i, 0, 0, memo), output);
 
         return output;
     }
