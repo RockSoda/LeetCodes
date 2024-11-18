1f6a708143339294e3d511c2ea7cb5f5f1619b43
Sigh <yxh644166727@gmail.com>
2024-11-18 13:18:09 -0500

    Time: 2 ms (48.21%), Space: 46.5 MB (8.25%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.cs
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cs
@@ -3,12 +3,12 @@ public class Solution
     public int[] Decrypt(int[] code, int k) 
     {
         var decoded = new int[code.Length];
-        if(k == 0) return decoded;
+        if (k == 0) return decoded;
         
-        for(int i = 0; i < decoded.Length; i++)
+        for (int i = 0; i < decoded.Length; i++)
         {
             int index = k > 0 ? i+1 : i-1;
-            for(int j = 0; j < Math.Abs(k); j++)
+            for (int j = 0; j < Math.Abs(k); j++)
             {
                 if (index < 0) index += decoded.Length;
                 decoded[i] += code[index % decoded.Length];
