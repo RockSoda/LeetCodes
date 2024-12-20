a0548cd50a9e5e5711cb71fe3bb1c1d6af74adc5
Sigh <yxh644166727@gmail.com>
2024-12-20 12:37:46 -0500

    Time: 45 ms (10%), Space: 67.4 MB (5%) - LeetHub

---
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cs
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    private void LoadMap(TreeNode node, Dictionary<int, List<int>> map, int level)
+    {
+        if (node == null) return;
+
+        if (!map.ContainsKey(level)) map[level] = new List<int>();
+        map[level].Add(node.val);
+
+        LoadMap(node.left, map, level+1);
+        LoadMap(node.right, map, level+1);
+    }
+
+    private void BuildTree(TreeNode node, Dictionary<int, List<int>> map, int level)
+    {
+        if (!map.ContainsKey(level) || map[level].Count == 0) return;
+
+        node.left = new TreeNode(map[level].First());
+        map[level].RemoveAt(0);
+        node.right = new TreeNode(map[level].First());
+        map[level].RemoveAt(0);
+
+        BuildTree(node.left, map, level+1);
+        BuildTree(node.right, map, level+1);
+    }
+
+    public TreeNode ReverseOddLevels(TreeNode root) 
+    {
+        var map = new Dictionary<int, List<int>>();
+        LoadMap(root, map, 0);
+        for(int i = 0; i < map.Count; i++)
+        {
+            if(i % 2 == 0) continue;
+            map[i].Reverse();
+        }
+        var output = new TreeNode(map[0].First());
+        BuildTree(output, map, 1);
+        return output;
+    }
+}
