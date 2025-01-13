ca54bc728300623b21b719e7df0298895797cde2
Sigh <yxh644166727@gmail.com>
2025-01-13 13:50:19 -0500

    Time: 5 ms (21.15%), Space: 49.3 MB (30.77%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cs
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cs
@@ -5,13 +5,14 @@ public class Solution
         var map = new int[26];
         foreach(var c in s) map[c-'a']++;
 
+        var len = 0;
         for(int i = 0; i < map.Length; i++)
         {
             var freq = map[i];
-            if(freq < 3) continue;
-            map[i] = freq % 2 == 0 ? 2 : 1;
+            if (freq >= 3) freq = freq % 2 == 0 ? 2 : 1;
+            len += freq;
         }
 
-        return map.Sum();
+        return len;
     }
 }
