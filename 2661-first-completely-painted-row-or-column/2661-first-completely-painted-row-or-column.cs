72ce13fae293d196a64e4dca804039daeaf2b66a
Sigh <yxh644166727@gmail.com>
2025-01-20 13:15:48 -0500

    Time: 67 ms (25%), Space: 84.6 MB (75%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cs
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cs
@@ -3,38 +3,24 @@ public class Solution
     public int FirstCompleteIndex(int[] arr, int[][] mat) 
     {
         var matMap = new Dictionary<int, (int, int)>();
-        var rowMap = new Dictionary<int, HashSet<int>>();
-        var colMap = new Dictionary<int, HashSet<int>>();
 
-        void AddToMap(Dictionary<int, HashSet<int>> map, int key, int val)
-        {
-            if(!map.ContainsKey(key)) map[key] = new HashSet<int>();
-            map[key].Add(val);
-        }
+        var rowFreq = new int[mat.Length];
+        Array.Fill(rowFreq, mat[0].Length);
+        var colFreq = new int[mat[0].Length];
+        Array.Fill(colFreq, mat.Length);
 
         for(int i = 0; i < mat.Length; i++)
         {
-            for(int j = 0; j < mat[i].Length; j++)
-            {
-                var curr = mat[i][j];
-                matMap[curr] = (i, j);
-                AddToMap(rowMap, i, curr);
-                AddToMap(colMap, j, curr);
-            }
-        }
-
-        bool IsEmptyAfterRemovedFromMap(Dictionary<int, HashSet<int>> map, int key, int val)
-        {
-            map[key].Remove(val);
-            return map[key].Count == 0;
+            for(int j = 0; j < mat[i].Length; j++) matMap[mat[i][j]] = (i, j);
         }
 
         for(int i = 0; i < arr.Length; i++)
         {
             var curr = arr[i];
             (int row, int col) = matMap[curr];
-            if(IsEmptyAfterRemovedFromMap(rowMap, row, curr)) return i;
-            if(IsEmptyAfterRemovedFromMap(colMap, col, curr)) return i;
+            
+            if(--rowFreq[row] == 0) return i;
+            if(--colFreq[col] == 0) return i;
         }
 
         return -1;
