fe18713addbe16f8f1c68c68ada6d35f8b5403ba
Sigh <yxh644166727@gmail.com>
2025-01-23 11:43:54 -0500

    Time: 3 ms (76.67%), Space: 59.8 MB (33.33%) - LeetHub

---
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cs
@@ -0,0 +1,48 @@
+public class Solution 
+{
+    private int _communicatedServers;
+
+    private void FlipCommunicated(int[][] grid, int i, int j)
+    {   
+        bool isCommunicating = false;
+        for(int row = 0; row < grid.Length; row++)
+        {
+            if(row == i || grid[row][j] == 0) continue;
+
+            if(grid[row][j] != -1) _communicatedServers++;
+            grid[row][j] = -1;
+            isCommunicating = true;
+        }
+
+        for(int col = 0; col < grid[i].Length; col++)
+        {
+            if(col == j || grid[i][col] == 0) continue;
+
+            if(grid[i][col] != -1) _communicatedServers++;
+            grid[i][col] = -1;
+            isCommunicating = true;
+        }
+
+        if(isCommunicating)
+        {
+            grid[i][j] = -1;
+            _communicatedServers++;
+        }
+    }
+
+    public int CountServers(int[][] grid) 
+    {
+        _communicatedServers = 0;
+        
+        for(int i = 0; i < grid.Length; i++)
+        {
+            for(int j = 0; j < grid[i].Length; j++)
+            {
+                if(grid[i][j] != 1) continue;
+                FlipCommunicated(grid, i, j);
+            }
+        }
+
+        return _communicatedServers;
+    }
+}
