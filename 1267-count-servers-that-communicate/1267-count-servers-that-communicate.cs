6fd97bebaf2fba75fdd4fde32736784fde64e70b
Sigh <yxh644166727@gmail.com>
2025-01-23 13:28:46 -0500

    Time: 4 ms (60%), Space: 59.4 MB (56.67%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cs
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cs
@@ -1,15 +1,13 @@
 public class Solution 
 {
-    private int _communicatedServers;
-
-    private void FlipCommunicated(int[][] grid, int i, int j)
+    private void FlipCommunicated(int[][] grid, int i, int j, ref int communicatedServers)
     {   
         bool isCommunicating = false;
         for(int row = 0; row < grid.Length; row++)
         {
             if(row == i || grid[row][j] == 0) continue;
 
-            if(grid[row][j] != -1) _communicatedServers++;
+            if(grid[row][j] != -1) communicatedServers++;
             grid[row][j] = -1;
             isCommunicating = true;
         }
@@ -18,7 +16,7 @@ public class Solution
         {
             if(col == j || grid[i][col] == 0) continue;
 
-            if(grid[i][col] != -1) _communicatedServers++;
+            if(grid[i][col] != -1) communicatedServers++;
             grid[i][col] = -1;
             isCommunicating = true;
         }
@@ -26,22 +24,22 @@ public class Solution
         if (!isCommunicating) return;
 
         grid[i][j] = -1;
-        _communicatedServers++;
+        communicatedServers++;
     }
 
     public int CountServers(int[][] grid) 
     {
-        _communicatedServers = 0;
+        int communicatedServers = 0;
         
         for(int i = 0; i < grid.Length; i++)
         {
             for(int j = 0; j < grid[i].Length; j++)
             {
                 if(grid[i][j] != 1) continue;
-                FlipCommunicated(grid, i, j);
+                FlipCommunicated(grid, i, j, ref communicatedServers);
             }
         }
 
-        return _communicatedServers;
+        return communicatedServers;
     }
 }
