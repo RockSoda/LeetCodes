ca9746f5a47a705bbca6209060fb9908f08fecfc
Sigh <yxh644166727@gmail.com>
2025-01-29 12:33:09 -0500

    Time: 33 ms (5.43%), Space: 50.9 MB (10.08%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cs
@@ -0,0 +1,62 @@
+public class Solution 
+{
+    public int[] FindRedundantConnection(int[][] edges) 
+    {
+        var map = new Dictionary<int, HashSet<int>>();
+        foreach(var edge in edges)
+        {
+            var num0 = edge[0];
+            var num1 = edge[1];
+            if(!map.ContainsKey(num0)) map[num0] = new HashSet<int>();
+            map[num0].Add(num1);
+
+            if(!map.ContainsKey(num1)) map[num1] = new HashSet<int>();
+            map[num1].Add(num0);
+        }
+
+        bool CanReach(int start, int end, HashSet<int> visited)
+        {
+            if(!map.ContainsKey(start)) return false;
+            if(map[start].Contains(end)) return true;
+            if(visited.Contains(start)) return false;
+
+            visited.Add(start);
+            bool isReached = false;
+            foreach(var num in map[start])
+            {
+                isReached = isReached || CanReach(num, end, visited);
+            }
+            return isReached;
+        }
+
+        void RemoveKeyVal(int num0, int num1)
+        {
+            map[num0].Remove(num1);
+            if(map[num0].Count == 0) map.Remove(num0);
+
+            map[num1].Remove(num0);
+            if(map[num1].Count == 0) map.Remove(num1);
+        }
+
+        void AddKeyVal(int num0, int num1)
+        {
+            if(!map.ContainsKey(num0)) map[num0] = new HashSet<int>();
+            map[num0].Add(num1);
+
+            if(!map.ContainsKey(num1)) map[num1] = new HashSet<int>();
+            map[num1].Add(num0);
+        }
+
+        for(int i = edges.Length - 1; i >= 0; i--)
+        {
+            var num0 = edges[i][0];
+            var num1 = edges[i][1];
+
+            RemoveKeyVal(num0, num1);
+            if(CanReach(num0, num1, new HashSet<int>())) return edges[i];
+            AddKeyVal(num0, num1);
+        }
+
+        return null;
+    }
+}
