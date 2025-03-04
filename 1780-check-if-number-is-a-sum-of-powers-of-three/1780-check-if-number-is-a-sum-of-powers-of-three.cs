434fccc37ed7273f80668241ba512d61486531c5
Sigh <yxh644166727@gmail.com>
2025-03-04 13:56:05 -0500

    Time: 1 ms (11.76%), Space: 31.7 MB (11.76%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cs
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cs
@@ -1,35 +1,28 @@
 public class Solution 
 {
-    public string ToTrenary(int value) 
+    private string ConvertFromDeci(int base1, int inputNum)
     {
-      if (value == 0) return "";
+        char reVal(int num)
+        {
+            if (num >= 0 && num <= 9)
+                return (char)(num + '0');
+            else
+                return (char)(num - 10 + 'A');
+        }
 
-      StringBuilder Sb = new StringBuilder();
-      Boolean signed = false;
-
-      if (value < 0) 
-      {
-        signed = true; 
-        value = -value;
-      }
-
-      while (value > 0) 
-      {
-        Sb.Insert(0, value % 3);
-        value /= 3;
-      }
-
-      if (signed) Sb.Insert(0, '-');
-
-      return Sb.ToString();
-    }
+        var sb = new StringBuilder();
     
-    public bool CheckPowersOfThree(int n) 
-    {
-        var ternary = ToTrenary(n);
-        
-        foreach(char c in ternary) if(c == '2') return false;
-        
-        return true;
+        // Convert input number is given 
+        // base by repeatedly dividing it
+        // by base and taking remainder
+        while (inputNum > 0)
+        {
+            sb.Insert(0, reVal(inputNum % base1));
+            inputNum /= base1;
+        }
+
+        return sb.ToString();
     }
+
+    public bool CheckPowersOfThree(int n) => !ConvertFromDeci(3, n).Any(c => c == '2');
 }
