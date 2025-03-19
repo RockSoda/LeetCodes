78ce3c0c4913269f17635a9d163bb9f2ccc03605
Sigh <yxh644166727@gmail.com>
2025-03-19 11:31:53 -0400

    Time: 7 ms (85%), Space: 61.4 MB (30%) - LeetHub

--- 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cs
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cs
@@ -4,11 +4,11 @@ public class Solution
     {
         void Flip(int idx)
         {
-            nums[idx] = nums[idx] == 0 ? 1 : 0;
-            nums[idx+1] = nums[idx+1] == 0 ? 1 : 0;
-            nums[idx+2] = nums[idx+2] == 0 ? 1 : 0;
+            nums[idx] ^= 1;
+            nums[idx+1] ^= 1;
+            nums[idx+2] ^= 1;
         }
-        
+
         int ans = 0;
 
         for(int i = 0; i < nums.Length-2; i++)
