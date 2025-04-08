36948593c00c0a3a4228fccfe1d8c5a167206543
Sigh <yxh644166727@gmail.com>
2025-04-08 11:20:56 -0400

    Time: 2 ms (84.62%), Space: 44.7 MB (64.1%) - LeetHub

--- 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.cs
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.cs
@@ -2,34 +2,11 @@ public class Solution
 {
     public int MinimumOperations(int[] nums) 
     {
-        var map = new Dictionary<int, int>();
-        foreach(var num in nums) map[num] = map.ContainsKey(num) ? map[num]+1 : 1;
-        
-        foreach(var key in map.Keys.ToList())
+        var set = new HashSet<int>();
+        for(int i = nums.Length-1; i >= 0; i--)
         {
-            if(map[key] == 1) map.Remove(key);
+            if(!set.Add(nums[i])) return i / 3 + 1;
         }
-
-        void CheckAndRemoveKeyFromMap(int key)
-        {
-            if(!map.ContainsKey(key)) return;
-            
-            if(--map[key] == 1) map.Remove(key);
-        }
-
-        var oprs = 0;
-        for(int i = 2; i < nums.Length; i += 3)
-        {
-            if(map.Count <= 0) break;
-
-            CheckAndRemoveKeyFromMap(nums[i-2]);
-            CheckAndRemoveKeyFromMap(nums[i-1]);
-            CheckAndRemoveKeyFromMap(nums[i]);
-
-            oprs++;
-        }
-
-        if(map.Count > 0) oprs++;
-        return oprs;
+        return 0;
     }
 }
