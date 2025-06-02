59718ca30bc62b04ce9ca1ac9de0094ea0be96e6
Sigh <yxh644166727@gmail.com>
2025-06-02 14:22:12 -0400

    Time: 4 ms (54.53%), Space: 48.4 MB (41.02%) - LeetHub

--- 0135-candy/0135-candy.cs
+++ 0135-candy/0135-candy.cs
@@ -3,19 +3,30 @@ public class Solution
     public int Candy(int[] ratings) 
     {
         var candies = new int[ratings.Length];
-        
+
         Array.Fill(candies, 1);
+        
+        var sum = ratings.Length;
         
         for(int i = ratings.Length-2; i >= 0; i--)
         {
-            if(ratings[i] > ratings[i+1]) candies[i] = candies[i+1]+1;
+            if(ratings[i] <= ratings[i+1]) continue;
+            
+            sum -= candies[i];
+            candies[i] = candies[i+1]+1;
+            sum += candies[i];
         }
         
         for(int i = 1; i < ratings.Length; i++)
         {
-            if(ratings[i] > ratings[i-1]) candies[i] = Math.Max(candies[i-1]+1, candies[i]);
+            if(ratings[i] <= ratings[i-1] || candies[i-1]+1 <= candies[i]) continue;
+
+            sum -= candies[i];
+            candies[i] = candies[i-1]+1;
+            sum += candies[i];
+
         }
-        
-        return candies.Sum();
+
+        return sum;
     }
 }
