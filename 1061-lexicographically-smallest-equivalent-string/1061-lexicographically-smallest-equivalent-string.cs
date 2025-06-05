1aa8b8067573a203f6bfb0fc04929b2d9c636702
Sigh <yxh644166727@gmail.com>
2025-06-05 16:22:09 -0400

    Time: 20 ms (14.29%), Space: 41 MB (66.67%) - LeetHub

---
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cs
@@ -0,0 +1,60 @@
+public class Solution 
+{
+    public string SmallestEquivalentString(string s1, string s2, string baseStr) 
+    {
+        var map = new bool[26][];
+        for(int i = 0; i < map.Length; i++)
+        {
+            map[i] = new bool[26];
+            map[i][i] = true;
+        }
+
+        var smallestMap = new int[26];
+
+        void LoadSmallestMap()
+        {
+            var visited = new HashSet<int>();
+            int FindSmallest(int key)
+            {
+                var min = key;
+                if(visited.Contains(key)) return min;
+
+                visited.Add(key);
+                if(smallestMap[key] != 0) return smallestMap[key];
+
+                for(int i = 0; i < map[key].Length; i++)
+                {
+                    if(!map[key][i]) continue;
+                    min = Math.Min(FindSmallest(i), min);
+                }
+                return min;
+            }
+
+            for(int i = 0; i < map.Length; i++)
+            {
+                smallestMap[i] = FindSmallest(i);
+                visited.Clear();
+            }
+        }
+
+        for(int i = 0; i < s1.Length; i++)
+        {
+            var key1 = s1[i]-'a';
+            var key2 = s2[i]-'a';
+
+            map[key1][key2] = true;
+            map[key2][key1] = true;
+        }
+
+        LoadSmallestMap();
+
+        var sb = new StringBuilder();
+        foreach(var c in baseStr)
+        {
+            var key = c-'a';
+            char smallestChar = (char)(smallestMap[key]+'a');
+            sb.Append(smallestChar);
+        }
+        return sb.ToString();
+    }
+}
