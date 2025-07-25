a63ff1c22bbe1e4eb977c8bbd65e1a374d795d24
Sigh <yxh644166727@gmail.com>
2025-07-25 14:50:17 -0400

    Time: 2 ms (72.41%), Space: 44.8 MB (10.34%) - LeetHub

--- 3487-maximum-unique-subarray-sum-after-deletion/3487-maximum-unique-subarray-sum-after-deletion.cs
+++ 3487-maximum-unique-subarray-sum-after-deletion/3487-maximum-unique-subarray-sum-after-deletion.cs
@@ -4,13 +4,19 @@ public class Solution
     {
         var sum = 0;
         var visited = new HashSet<int>();
+        var max = nums.First();
         foreach(var num in nums)
         {
-            if(num < 0) continue;
+            if(num < 0)
+            {
+                max = Math.Max(max, num);
+                continue;
+            }
+
             if(visited.Add(num)) sum += num;
         }
 
-        if(visited.Count == 0) return nums.Max();
+        if(visited.Count == 0) return max;
 
         return sum;
     }
