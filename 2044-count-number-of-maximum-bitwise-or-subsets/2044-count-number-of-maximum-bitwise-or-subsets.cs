4d01a7a18ca39f28ff999dda1c583d721237e957
Sigh <yxh644166727@gmail.com>
2025-07-28 14:46:00 -0400

    Time: 10 ms (83.87%), Space: 41.9 MB (20.97%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cs
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cs
@@ -4,21 +4,22 @@ public class Solution
     {
         var bitwiseOr = 0;
         foreach(var num in nums) bitwiseOr |= num;
-        
-        int n = nums.Length;
-        int totalSubsets = 1 << n; // Equivalent to Math.Pow(2, n)
-        int numOfSubsets = 0;
-        for (int i = 0; i < totalSubsets; i++)
+        var numOfSubsets = 0;
+
+        void DFS(int idx, int currOr)
         {
-            var currOr = 0;
-            for (int j = 0; j < n; j++)
+            if(idx == nums.Length)
             {
-                // Check if the j-th bit of 'i' is set
-                if ((i & (1 << j)) != 0) currOr |= nums[j];
+                if(currOr == bitwiseOr) numOfSubsets++;
+                return;
             }
+
+            DFS(idx+1, currOr | nums[idx]);
             
-            if(currOr == bitwiseOr) numOfSubsets++;
+            DFS(idx+1, currOr);
         }
+
+        DFS(0, 0);
 
         return numOfSubsets;
     }
