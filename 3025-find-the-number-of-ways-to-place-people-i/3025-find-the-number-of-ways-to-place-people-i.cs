12a23e336bf8890ef497fa403a4ea0fde068da0d
Sigh <yxh644166727@gmail.com>
2025-09-02 17:44:37 -0400

    Time: 66 ms (12.5%), Space: 58 MB (12.5%) - LeetHub

--- 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cs
+++ 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cs
@@ -2,34 +2,30 @@ public class Solution
 {
     public int NumberOfPairs(int[][] points) 
     {
-        bool IsUpperLeft(int[] upperleft, int[] lowerright) => upperleft[0] <= lowerright[0] && 
-                                                               upperleft[1] >= lowerright[1];
+        bool IsLegalUpperLeft(int[] upperLeft, int[] lowerRight) 
+        {
+            if(upperLeft[0] > lowerRight[0] || upperLeft[1] < lowerRight[1]) return false;
+            
+            if(points.Any(point => (point[0] != upperLeft[0] || point[1] != upperLeft[1]) && 
+                                    (point[0] != lowerRight[0] || point[1] != lowerRight[1]) && 
+                                    ContainsTarget(upperLeft, lowerRight, point))) return false;
+            
+            return true;
+        }
 
-        bool ContainsTarget(int[] upperleft, int[] lowerright, int[] target) => upperleft[0] <= target[0] && 
-                                                                                lowerright[0] >= target[0] && 
-                                                                                upperleft[1] >= target[1] && 
-                                                                                lowerright[1] <= target[1];
+        bool ContainsTarget(int[] upperLeft, int[] lowerRight, int[] target) => upperLeft[0] <= target[0] && 
+                                                                                lowerRight[0] >= target[0] && 
+                                                                                upperLeft[1] >= target[1] && 
+                                                                                lowerRight[1] <= target[1];
         
-        var pairs = new List<(int[], int[])>();
+        var numOfPairs = 0;
         for(int i = 0; i < points.Length; i++)
         {
             for(int j = i+1; j < points.Length; j++)
             {
-                if(IsUpperLeft(points[i], points[j])) pairs.Add((points[i], points[j]));
-                else if(IsUpperLeft(points[j], points[i])) pairs.Add((points[j], points[i]));
+                if(IsLegalUpperLeft(points[i], points[j]) || IsLegalUpperLeft(points[j], points[i])) numOfPairs++;
             }
         }
-
-        foreach((int[] upperLeft, int[] lowerRight) in pairs.ToList())
-        {
-            foreach(var point in points)
-            {
-                if(point[0] == upperLeft[0] && point[1] == upperLeft[1] || 
-                   point[0] == lowerRight[0] && point[1] == lowerRight[1]) continue;
-
-                if(ContainsTarget(upperLeft, lowerRight, point)) pairs.Remove((upperLeft, lowerRight));
-            }
-        }
-        return pairs.Count;
+        return numOfPairs;
     }
 }
