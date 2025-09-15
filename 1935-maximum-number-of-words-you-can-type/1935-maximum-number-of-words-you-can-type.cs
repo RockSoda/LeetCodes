a2a7254d7a741a55bbef43f84487c9428785958d
Sigh <yxh644166727@gmail.com>
2025-09-15 16:21:08 -0400

    Time: 2 ms (33.33%), Space: 39.9 MB (83.33%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cs
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cs
@@ -6,11 +6,9 @@ public class Solution
         foreach (var c in brokenLetters) brokenChars[c-'a'] = true;
         var aryOfTxt = text.Split(' ');
         var output = aryOfTxt.Length;
+
+        Array.ForEach(aryOfTxt, txt => output -= txt.Any(c => brokenChars[c-'a']) ? 1 : 0);
         
-        foreach (var txt in aryOfTxt)
-        {
-            if (txt.Any(c => brokenChars[c-'a'])) output--;
-        }
         return output;
     }
 }
