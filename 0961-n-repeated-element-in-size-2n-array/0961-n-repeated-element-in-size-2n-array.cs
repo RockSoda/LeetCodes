f1a0dd2e43fc1773021bb7147a3499b0632e6190
Sigh <yxh644166727@gmail.com>
2026-01-02 15:19:40 -0500

    Time: 2 ms (61.67%), Space: 50.5 MB (60%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cs
@@ -0,0 +1,11 @@
+public class Solution 
+{
+    public int RepeatedNTimes(int[] nums) 
+    {
+        var set = new HashSet<int>();
+        foreach(var num in nums)
+            if(!set.Add(num)) return num;
+        
+        return -1;
+    }
+}
