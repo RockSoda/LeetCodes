287f3ec0fbfb67f6555e2fae6ba2f2a85c34791e
Sigh <yxh644166727@gmail.com>
2026-02-12 14:23:27 -0500

    Time: 0 ms (100%), Space: 41.8 MB (26.42%) - LeetHub

--- 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.cs
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.cs
@@ -1,5 +1,23 @@
-public class Solution {
-    public int MaxAscendingSum(int[] nums) {
-        
+public class Solution 
+{
+    public int MaxAscendingSum(int[] nums) 
+    {
+        int prev = nums.First(), maxSum = prev, currSum = prev;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            int curr = nums[i];
+
+            if (prev < curr) currSum += curr;
+            else
+            {
+                maxSum = Math.Max(maxSum, currSum);
+                currSum = curr;
+            }
+            
+            prev = curr;
+        }
+
+        return Math.Max(maxSum, currSum);
     }
 }
