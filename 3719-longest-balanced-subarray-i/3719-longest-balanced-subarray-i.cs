3473bd0a6746d362cbd05ed666c3f83da44dea36
Sigh <yxh644166727@gmail.com>
2026-02-12 14:21:12 -0500

    Time: 1687 ms (5.3%), Space: 58.4 MB (40.91%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cs
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cs
@@ -5,7 +5,8 @@ public class Solution
         int maxLen = 0;
         for(int i = 0; i < nums.Length; i++)
         {
-            var odd = new HashSet<int>(), even = new HashSet<int>();
+            var odd = new HashSet<int>();
+            var even = new HashSet<int>();
             for(int j = i; j < nums.Length; j++)
             {
                 if(nums[j] % 2 == 0) even.Add(nums[j]);
@@ -14,7 +15,7 @@ public class Solution
                 if(odd.Count() == even.Count()) maxLen = Math.Max(maxLen, j-i+1);
             }
         }
-        
+
         return maxLen;
     }
 }
