a1dffdbd81a9e44fcb4d5a63b73fbe559fde6aef
Sigh <yxh644166727@gmail.com>
2026-02-12 14:24:31 -0500

    Time: 105 ms (43.48%), Space: 61.8 MB (79.35%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cs
+++ 0729-my-calendar-i/0729-my-calendar-i.cs
@@ -1,57 +1,22 @@
-public class MyCalender
+public class MyCalendar 
 {
-	readonly List<int[]> meetings;
-	public MyCalender()
-	{
-		meetings = new List<int[]>();
-	}
+    private List<(int start, int end)> intervals;
 
-	/// <summary>
-	/// 
-	/// </summary>
-	/// <param name="start"></param>
-	/// <param name="end"></param>
-	/// <returns></returns>
-	public bool Book(int start, int end)
-	{
-		if (meetings.Count == 0)
-		{
-			meetings.Add(new int[] { start, end });
-			return true;
-		}
+    public MyCalendar() 
+    {
+        intervals = new List<(int, int)>();
+    }
+    
+    public bool Book(int start, int end) 
+    {
+        foreach(var booked in intervals) 
+        {
+            if (Math.Max(booked.start, start) < Math.Min(booked.end, end)) return false;
+        }
 
-		// get the existing schedule list and sort
-		var existing = meetings.Select(x => x).ToArray();
-		Array.Sort(existing, (a,b)=> a[0].CompareTo(b[0]));
-		int length = existing.Length;
-		int i;
-		int existingStart = 0;
-		int existingEnd = 0;
-		for (i = 0; i < length; i++)
-		{
-			existingStart = existing[i][0];
-			existingEnd = existing[i][1];
-			if (existingEnd > start) break;
-		}
-
-		// if we reach at end of the array and there are no meetings which end before proposed start
-		if (i == length)
-		{
-			meetings.Add(new int[] { start, end });
-			return true;
-		}
-
-		// start time and existing start times are same return false
-		if (start == existingStart) return false;
-
-		// if new proposed start time is in between existing scheduled meeting time.
-		if (start > existingStart && start < existingEnd) return false;
-		else if (end > existingStart) return false; // if new proposed end time is in between existing scheduled meeting time.
-
-		// good to schedule a meeting for the proposed time
-		meetings.Add(new int[] { start, end });
-		return true;
-	}
+        intervals.Add((start,end));
+        return true;
+    }
 }
 
 /**
