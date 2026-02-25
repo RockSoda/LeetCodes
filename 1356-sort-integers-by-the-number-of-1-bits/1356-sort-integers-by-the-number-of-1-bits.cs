18578c5a763c2842b419a7b6c7fac0695fba9499
Sigh <yxh644166727@gmail.com>
2026-02-25 17:20:30 -0500

    Time: 148 ms (6.12%), Space: 44.7 MB (100%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cs
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cs
@@ -1,23 +1,39 @@
 public class Solution 
 {
+    private string ToBin(int num) => Convert.ToString(num, 2);
+    
+    private int GetOccurance(string str, char c)
+    {
+        int counter = 0;
+        foreach(char ch in str) if(ch == c) counter++;
+        
+        return counter;
+    }
+    
     public int[] SortByBits(int[] arr) 
     {
         Array.Sort(arr);
-
-        var map = new Dictionary<int, List<int>>();
-        foreach(var num in arr)
-        {
-            var bits = System.Numerics.BitOperations.PopCount((uint)num);
-            if(!map.ContainsKey(bits)) map[bits] = new List<int>();
-            map[bits].Add(num);
-        }
-
-        var output = new List<int>();
-
-        var keys = map.Keys.ToList();
-        keys.Sort();
-        foreach(var key in keys) output.AddRange(map[key]);
+        var map = new SortedDictionary<int, List<int>>();
         
-        return output.ToArray();
+        foreach(int num in arr)
+        {
+            int numOfOnes = GetOccurance(ToBin(num), '1');
+            if(map.ContainsKey(numOfOnes)) map[numOfOnes].Add(num);
+            else map.Add(numOfOnes, new List<int>{ num });
+        }
+        
+        var output = new int[arr.Length];
+        
+        int index = 0;
+        foreach(var kvp in map)
+        {
+            foreach(int num in kvp.Value)
+            {
+                output[index] = num;
+                index++;
+            }
+        }
+        
+        return output;
     }
 }
