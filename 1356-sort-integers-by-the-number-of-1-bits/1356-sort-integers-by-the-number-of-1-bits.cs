9d8df35ef79febac3a0d01ed3155c4e6d9a79ff7
Sigh <yxh644166727@gmail.com>
2026-02-25 17:16:24 -0500

    Time: 6 ms (81.63%), Space: 49.1 MB (63.27%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cs
@@ -0,0 +1,23 @@
+public class Solution 
+{
+    public int[] SortByBits(int[] arr) 
+    {
+        Array.Sort(arr);
+
+        var map = new Dictionary<int, List<int>>();
+        foreach(var num in arr)
+        {
+            var bits = System.Numerics.BitOperations.PopCount((uint)num);
+            if(!map.ContainsKey(bits)) map[bits] = new List<int>();
+            map[bits].Add(num);
+        }
+
+        var output = new List<int>();
+
+        var keys = map.Keys.ToList();
+        keys.Sort();
+        foreach(var key in keys) output.AddRange(map[key]);
+        
+        return output.ToArray();
+    }
+}
