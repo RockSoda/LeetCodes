77fe850de7f7c9be38ab5097e07c0d84ecfa7434
Sigh <yxh644166727@gmail.com>
2026-03-11 14:36:08 -0400

    Time: 0 ms (100%), Space: 29 MB (60%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cs
@@ -0,0 +1,21 @@
+public class Solution 
+{
+    public int FindComplement(int n)
+    {
+        int GetBitWidth(int input)
+        {
+            var bitWidth = 0;
+            while(input > 0)
+            {
+                bitWidth++;
+                input = input >> 1;
+            }
+            return bitWidth;
+        }
+
+        if(n == 0) return 1;
+        
+        var mask = (1 << GetBitWidth(n)) - 1;
+        return n ^ mask;
+    }
+}
