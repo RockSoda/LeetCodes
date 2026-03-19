a0e4cd2824fbe6c6dd6f639c5cec312f2e072b35
Sigh <yxh644166727@gmail.com>
2026-03-19 15:57:53 -0400

    Time: 30 ms (100%), Space: 101.4 MB (0%) - LeetHub

---
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cs
@@ -0,0 +1,51 @@
+public class Solution 
+{
+    public int NumberOfSubmatrices(char[][] grid) 
+    {
+        (int x, int y)[][] prefixSum = new (int, int)[grid.Length][];
+        for(int i = 0; i < grid.Length; i++) prefixSum[i] = new (int, int)[grid[i].Length];
+
+        (int, int) GetXY(char c)
+        {
+            if(c == 'X') return (1, 0);
+            else if(c == 'Y') return (0, 1);
+            else return (0, 0);
+        }
+
+        prefixSum[0][0] = GetXY(grid[0][0]);
+        
+        for(int i = 1; i < grid.Length; i++)
+        {
+            (int x, int y) = GetXY(grid[i][0]);
+            prefixSum[i][0] = (prefixSum[i-1][0].x + x, prefixSum[i-1][0].y + y);
+            
+        }
+        
+        for(int j = 1; j < grid[0].Length; j++)
+        {
+            (int x, int y) = GetXY(grid[0][j]);
+            prefixSum[0][j] = (prefixSum[0][j-1].x + x, prefixSum[0][j-1].y + y);
+        }
+
+
+        for(int i = 1; i < grid.Length; i++)
+        {
+            for(int j = 1; j < grid[i].Length; j++)
+            {
+                (int x, int y) = GetXY(grid[i][j]);
+
+                prefixSum[i][j] = (x + prefixSum[i][j-1].x + prefixSum[i-1][j].x - prefixSum[i-1][j-1].x, y + prefixSum[i][j-1].y + prefixSum[i-1][j].y - prefixSum[i-1][j-1].y);
+            }
+        }
+        var output = 0;
+        foreach(var row in prefixSum)
+        {
+            foreach((int x, int y) in row)
+            {
+                if(x == y && x != 0) output++;
+            }
+        }
+
+        return output;
+    }
+}
