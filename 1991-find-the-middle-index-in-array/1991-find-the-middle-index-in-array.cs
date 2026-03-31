29fd56e36b9fb8841708e96a7af1f016ede1fd31
Sigh <yxh644166727@gmail.com>
2026-03-31 12:10:07 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cs
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cs
@@ -2,14 +2,15 @@ public class Solution
 {
     public int FindMiddleIndex(int[] nums) 
     {
-        var sum = nums.Sum();
-        var left = 0;
+        var suffixSum = new int[nums.Length];
+        suffixSum[nums.Length-1] = nums[nums.Length-1];
+        for(int i = nums.Length-2; i >= 0; i--) suffixSum[i] += suffixSum[i+1] + nums[i];
+
+        var sum = 0;
         for(int i = 0; i < nums.Length; i++)
         {
-            var num = nums[i];
-            sum -= num;
-            if(left == sum) return i;
-            left += num;
+            sum += nums[i];
+            if(sum == suffixSum[i]) return i;
         }
 
         return -1;
